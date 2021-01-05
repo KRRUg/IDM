eba04853d1475c0ece937aafb24541d71d581c8a
Thomas Hader <thader@drei.at>
2021-01-05 19:37:07 +0100

    Added test framework for IDM

---
+++ tests/UserControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserControllerTest extends AbstractControllerTest
+{
+    public function testUserRequest()
+    {
+        $uuid = Uuid::fromInteger(1)->toString();
+        $this->client->request('GET', '/api/users/' . $uuid);
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals("application/json", $response->headers->get('Content-Type'));
+
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result, "No valid JSON returned.");
+        $this->assertArrayHasKey("uuid", $result);
+        $this->assertArrayHasKey("email", $result);
+        $this->assertArrayHasKey("nickname", $result);
+        $this->assertArrayHasKey("firstname", $result);
+        $this->assertArrayHasKey("surname", $result);
+        $this->assertArrayHasKey("postcode", $result);
+        $this->assertArrayHasKey("city", $result);
+        $this->assertArrayHasKey("street", $result);
+        $this->assertArrayHasKey("country", $result);
+        $this->assertArrayHasKey("phone", $result);
+        $this->assertArrayHasKey("gender", $result);
+        $this->assertArrayHasKey("emailConfirmed", $result);
+        $this->assertArrayHasKey("isSuperadmin", $result);
+        $this->assertArrayHasKey("steamAccount", $result);
+        $this->assertArrayHasKey("registeredAt", $result);
+        $this->assertArrayHasKey("modifiedAt", $result);
+        $this->assertArrayHasKey("hardware", $result);
+        $this->assertArrayHasKey("infoMails", $result);
+        $this->assertArrayHasKey("statements", $result);
+        $this->assertArrayHasKey("birthdate", $result);
+        $this->assertArrayNotHasKey("password", $result);
+
+        $this->assertEquals("User 1", $result['nickname']);
+        $this->assertEquals("user1@localhost.local", $result['email']);
+        $this->assertFalse($result['isSuperadmin']);
+    }
+}
