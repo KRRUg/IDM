9e957dc6a01d3cd27dbdec297f86470b3a846555
Thomas Hader <thader@drei.at>
2021-01-07 18:34:40 +0100

    Added Tests for clan join/leave and admin add/remove

--- tests/ClanControllerMemberTest.php
+++ tests/ClanControllerMemberTest.php
@@ -4,12 +4,14 @@
 namespace App\Tests;
 
 
+use App\Transfer\UuidObject;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Response;
+use function foo\func;
 
 class ClanControllerMemberTest extends AbstractControllerTest
 {
-    public function testGetMembers()
+    public function testGetMembersSuccess()
     {
         $uuid = Uuid::fromInteger(1001)->toString();
         $this->client->request('GET', '/api/clans/' . $uuid . "/users");
@@ -21,10 +23,10 @@ class ClanControllerMemberTest extends AbstractControllerTest
 
         $result = json_decode($response->getContent(), true);
         $this->assertIsArray($result);
-        $this->assertEquals(2, sizeof($result));
+        $this->check_uuid_array($result, [1, 2]);
     }
 
-    public function testAddMember()
+    public function testAddMemberSuccess()
     {
         $uuid = Uuid::fromInteger(1003)->toString();
         $user = Uuid::fromInteger(18)->toString();
@@ -54,10 +56,10 @@ JSON;
         $this->assertJson($response->getContent());
         $result = json_decode($response->getContent(), true);
         $this->assertIsArray($result);
-        $this->assertEquals(1, sizeof($result));
+        $this->check_uuid_array($result, [18]);
     }
 
-    public function testAddAdmin()
+    public function testAddAdminSuccess()
     {
         $uuid = Uuid::fromInteger(1003)->toString();
         $user = Uuid::fromInteger(18)->toString();
@@ -78,7 +80,8 @@ JSON;
         $this->assertJson($response->getContent());
         $result = json_decode($response->getContent(), true);
         $this->assertIsArray($result);
-        $this->assertEquals(1, sizeof($result));
+        $this->check_uuid_array($result, [18]);
+
 
         // check if user is actually a member now
         $this->client->request('GET', '/api/clans/' . $uuid . "/users");
@@ -87,6 +90,142 @@ JSON;
         $this->assertJson($response->getContent());
         $result = json_decode($response->getContent(), true);
         $this->assertIsArray($result);
-        $this->assertEquals(1, sizeof($result));
+        $this->check_uuid_array($result, [18]);
+    }
+
+    public function testRemoveAdminSuccess()
+    {
+        $uuid = Uuid::fromInteger(1002)->toString();
+        $user = Uuid::fromInteger(3)->toString();
+
+        $this->client->request('DELETE', '/api/clans/' . $uuid . "/admins/" . $user);
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->getStatusCode());
+
+        // check if user is an admin now
+        $this->client->request('GET', '/api/clans/' . $uuid . "/admins");
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result);
+        $this->assertEquals(0, sizeof($result));
+
+        // check if user is still a member
+        $this->client->request('GET', '/api/clans/' . $uuid . "/users");
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result);
+        $this->check_uuid_array($result, [2,3,4]);
+    }
+
+    public function testRemoveAdminFailNotAdminButUser()
+    {
+        $uuid = Uuid::fromInteger(1002)->toString();
+        $user = Uuid::fromInteger(2)->toString();
+
+        $this->client->request('DELETE', '/api/clans/' . $uuid . "/admins/" . $user);
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+
+        // check if admins didn't change
+        $this->client->request('GET', '/api/clans/' . $uuid . "/admins");
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result);
+        $this->check_uuid_array($result, [3]);
+
+        // check if user is still a member
+        $this->client->request('GET', '/api/clans/' . $uuid . "/users");
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result);
+        $this->check_uuid_array($result, [2,3,4]);
+    }
+
+    public function testRemoveAdminSuccessFromClan()
+    {
+        $uuid = Uuid::fromInteger(1002)->toString();
+        $user = Uuid::fromInteger(3)->toString();
+
+        $this->client->request('DELETE', '/api/clans/' . $uuid . "/users/" . $user);
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->getStatusCode());
+
+        // check if user is no admin any more
+        $this->client->request('GET', '/api/clans/' . $uuid . "/admins");
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result);
+        $this->assertEquals(0, sizeof($result));
+
+        // check if user is no member any more
+        $this->client->request('GET', '/api/clans/' . $uuid . "/users");
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result);
+        $this->check_uuid_array($result, [2,4]);
+    }
+
+    public function testAddAdminSuccessPromoteUser()
+    {
+        $uuid = Uuid::fromInteger(1002)->toString();
+        $user = Uuid::fromInteger(2)->toString();
+        $data = <<<JSON
+{
+    "uuid": "{$user}"
+}
+JSON;
+
+        $this->client->request('POST', '/api/clans/' . $uuid . "/admins", [], [], ['CONTENT_TYPE' => 'application/json'], $data);
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->getStatusCode());
+
+        // check if user is admin now
+        $this->client->request('GET', '/api/clans/' . $uuid . "/admins");
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result);
+        $this->check_uuid_array($result, [2,3]);
+
+        // check if user is still member
+        $this->client->request('GET', '/api/clans/' . $uuid . "/users");
+        $response = $this->client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $result = json_decode($response->getContent(), true);
+        $this->assertIsArray($result);
+        $this->check_uuid_array($result, [2,3,4]);
+    }
+
+    /**
+     * @param array[] $array array of arrays which contain one uuid value to search for
+     * @param int[] $expected UUIDs to check for
+     */
+    private function check_uuid_array(array $array, array $expected)
+    {
+        $this->assertSameSize($array, $expected);
+        $expected = array_map(function (int $i) { return Uuid::fromInteger($i)->toString();}, $expected);
+        foreach ($array as $item) {
+            $this->assertIsArray($item);
+            $this->assertEquals(1, sizeof($item));
+            $this->assertArrayHasKey("uuid", $item);
+        }
+        $array = array_map(function ($a) { return $a['uuid']; }, $array);
+        foreach ($expected as $ex) {
+            $this->assertContains($ex, $array);
+        }
     }
 }
